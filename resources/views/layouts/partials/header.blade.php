efe4773426ce2e6ecf1976a975a20743113b97a0
prasadiniherath <prasadinichamalka@gmail.com>
2021-06-25 12:11:28 +0530

    modify

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -26,10 +26,10 @@
             <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
                 <span class="dropdown-item dropdown-header">15 Notifications</span>
                 <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item">
-                    <i class="fas fa-envelope mr-2"></i> 4 new messages
-                    <span class="float-right text-muted text-sm">3 mins</span>
-                </a>
+{{--                <a href="#" class="dropdown-item">--}}
+{{--                    <i class="fas fa-envelope mr-2"></i> 4 new messages--}}
+{{--                    <span class="float-right text-muted text-sm">3 mins</span>--}}
+{{--                </a>--}}
                 <div class="dropdown-divider"></div>
                 <a href="#" class="dropdown-item">
                     <i class="fas fa-file mr-2"></i> 3 new reports
